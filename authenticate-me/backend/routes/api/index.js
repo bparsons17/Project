ecf5d23a1796257ebc4e2e78108751da9b40b224
brandon <bparsons17@liberty.edu>
2021-01-22 18:55:58 -0500

    completed log out route

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie } = require("../../utils/auth.js");
 const { User } = require("../../db/models");
 const { restoreUser } = require("../../utils/auth.js");
 const { requireAuth } = require("../../utils/auth.js");
 
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
