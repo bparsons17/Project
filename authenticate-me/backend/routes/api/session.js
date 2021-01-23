a886246081f20846ee9cbb0fca5de8f391d9a7ec
brandon <bparsons17@liberty.edu>
2021-01-23 14:47:41 -0500

    added validation errors

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const { User } = require("../../db/models");
 const router = express.Router();
 
-router.post("/", asyncHandler(async (req, res, next) => {
+
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
+
+router.post("/", validateLogin, asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
@@ -27,6 +42,14 @@ router.post("/", asyncHandler(async (req, res, next) => {
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
+});
+router.get("/", restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject(),
+    });
+  } else return res.json({});
 });
 
 
