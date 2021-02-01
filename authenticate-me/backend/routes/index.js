ed785572f024d95f264d42fd09bbe43201bb9cf5
brandon <bparsons17@liberty.edu>
2021-02-01 09:13:31 -0500

    space added

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -34,6 +34,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 
+
 // router.get("/hello/world", function (req, res) {
 //   res.cookie("XSRF-TOKEN", req.csrfToken());
 //   res.send("Hello World!");
